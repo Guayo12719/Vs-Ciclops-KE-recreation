14eb1219625c23edad0ce9ef838d7786625da966
Guayo12719 <96029111+Guayo12719@users.noreply.github.com>
2022-05-12 02:27:50 +0200

    Add files via upload

--- source/Character.hx
+++ source/Character.hx
@@ -30,39 +30,29 @@ class Character extends FlxSprite
 
 		switch (curCharacter)
 		{
-			case 'gf':
-				// GIRLFRIEND CODE
-				tex = Paths.getSparrowAtlas('characters/GF_assets');
+			case 'sonic':
+				// SONIC CODE
+				tex = Paths.getSparrowAtlas('characters/SPEED_FULL_ASS_SETS');
 				frames = tex;
-				animation.addByPrefix('cheer', 'GF Cheer', 24, false);
-				animation.addByPrefix('singLEFT', 'GF left note', 24, false);
-				animation.addByPrefix('singRIGHT', 'GF Right Note', 24, false);
-				animation.addByPrefix('singUP', 'GF Up Note', 24, false);
-				animation.addByPrefix('singDOWN', 'GF Down Note', 24, false);
-				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
-				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-				animation.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], "", 24);
-				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
-				animation.addByPrefix('scared', 'GF FEAR', 24);
+				animation.addByPrefix('idle', 'SPEED_IDLE', 24, false);
+				animation.addByPrefix('singLEFT', 'SPEED_LEFT', 24, false);
+				animation.addByPrefix('singRIGHT', 'SPEED_RIGHT', 24, false);
+				animation.addByPrefix('singUP', 'SPEED_UP', 24, false);
+				animation.addByPrefix('singDOWN', 'SPEED_DOWN', 24, false);
+				animation.addByIndices('stare', 'stareidle', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 
-				addOffset('cheer');
-				addOffset('sad', -2, -2);
-				addOffset('danceLeft', 0, -9);
-				addOffset('danceRight', 0, -9);
+				addOffset('idle');
 
-				addOffset("singUP", 0, 4);
-				addOffset("singRIGHT", 0, -20);
-				addOffset("singLEFT", 0, -19);
-				addOffset("singDOWN", 0, -20);
-				addOffset('hairBlow', 45, -8);
-				addOffset('hairFall', 0, -9);
+				addOffset("SPEED_UP", 0, 4);
+				addOffset("SPEED_RIGHT", 0, -20);
+				addOffset("SPEED_LEFT", 0, -19);
+				addOffset("SPEED_DOWN", 0, -20);
+				addOffset('SPEED_IDLE', 45, -8);
+				addOffset('stareidle', 0, -9);
 
-				addOffset('scared', -2, -17);
+				playAnim('SPEED_IDLE');
 
-				playAnim('danceRight');
-
-			case 'gf-christmas':
+			case 'gf chistamst':
 				tex = Paths.getSparrowAtlas('characters/gfChristmas');
 				frames = tex;
 				animation.addByPrefix('cheer', 'GF Cheer', 24, false);
